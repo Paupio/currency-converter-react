68ec7a21ca41521243ad0426d1cf124910294073
Paulina Piotrzkowska <paulina.piotrzkowska@gmail.com>
2023-11-06 14:12:01 +0100

    changed

---
+++ src/App/Form/index.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { currencies } from "../currencies";
+import { Result } from "./Result";
+import "./style.css";
+
+export const Form = ({ calculateResult, result }) => {
+    const [currency, setCurrency] = useState(currencies[0].short);
+    const [amount, setAmount] = useState("");
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        calculateResult(currency, amount);
+    }
+
+    return (
+        <form className="form" onSubmit={onSubmit}>
+            <h1 className="form__header">
+                Przelicznik walut
+            </h1>
+            <fieldset className="form__fieldset">
+                <legend className="form__legend">
+                    Kalkulator
+                </legend>
+                <p>
+                    <label className="container">
+                        <span className="form__labelText">
+                            Kwota w zł*:
+                        </span>
+                        <input
+                            autoFocus
+                            className="form__field"
+                            value={amount}
+                            onChange={({ target }) => setAmount(target.value)}
+                            placeholder="Wpisz kwotę w złotówkach"
+                            type="number"
+                            required
+                        />
+                    </label>
+                </p>
+                <p>
+                    <label className="container">
+                        <span className="form__labelText">
+                            Wybierz walutę:
+                        </span>
+                        <select
+                            className="form__field"
+                            value={currency}
+                            onChange={({ target }) => setCurrency(target.value)}
+                        >
+                            {currencies.map((currency => (
+                                <option
+                                    key={currency.short}
+                                    value={currency.short}
+                                >
+                                    {currency.name}
+                                </option>
+                            )))}
+                        </select>
+                    </label>
+                </p>
+                <p>
+                    <button className="form__button">
+                        <strong>Przelicz</strong>
+                    </button>
+                </p>
+                <p>
+                    <i>Pola wymagane oznaczone są *</i>
+                </p>
+                <Result result={result} />
+            </fieldset>
+        </form>
+    );
+};
---
+++ src/App/currencies.js
@@ -0,0 +1,12 @@
+export const currencies = [
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.4607,
+    },
+    {
+        short: "USD",
+        name: "Dolar amerykański",
+        rate: 4.3718,
+    },
+];
