d8e5929870623df1b2912213dbda4966c97a6ca0
Paulina Piotrzkowska <paulina.piotrzkowska@gmail.com>
2024-01-13 00:27:42 +0100

    added styled.js

---
+++ src/App/Form/Clock/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledClock = styled.div`
+    display: flex;
+    flex-direction: row-reverse;
+    padding-bottom: 20px;
+    font-family: monospace;
+    font-size: 14px;
+    color: black;
+`;
---
+++ src/App/Form/Result/styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const StyledResult = styled.p`
+    text-align: center;
+    font-size: 30px;
+`;
