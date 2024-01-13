6658b96519a0d0ccbe94857ad570c058243b76f8
Paulina Piotrzkowska <paulina.piotrzkowska@gmail.com>
2024-01-13 20:07:48 +0100

    formatting

--- src/App/Form/Clock/index.js
+++ src/App/Form/Clock/index.js
@@ -2,13 +2,13 @@ import { useCurrentDate } from "./useCurrentDate";
 import { StyledClock } from "./styled";
 
 const formatDate = (date) => date.toLocaleString(undefined, {
-        weekday: "long",
-        hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-        day: "numeric",
-        month: "long",
-    });
+    weekday: "long",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    day: "numeric",
+    month: "long",
+});
 
 export const Clock = () => {
     const date = useCurrentDate();
--- src/App/Form/Clock/useCurrentDate.js
+++ src/App/Form/Clock/useCurrentDate.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 export const useCurrentDate = () => {
-const [date, setDate] = useState(new Date());
+    const [date, setDate] = useState(new Date());
 
     useEffect(() => {
         const intervalId = setInterval(() => {
